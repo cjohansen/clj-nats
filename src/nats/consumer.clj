3fff738a19ab66e3d7266ba9ae4d275c4ed65f53
Christian Johansen <christian@cjohansen.no>
2024-04-12 17:13:38 +0200

    Reorganize and document the consumer namespace

--- src/nats/consumer.clj
+++ src/nats/consumer.clj
@@ -9,12 +9,14 @@
            (io.nats.client.impl AckType)
            (java.time Instant)))
 
+;; Enums as keywords
+
 (def ack-policies
   {:nats.ack-policy/all AckPolicy/All
    :nats.ack-policy/explicit AckPolicy/Explicit
    :nats.ack-policy/none AckPolicy/None})
 
-(def ack-policy->k (set/map-invert ack-policies))
+(def ^:no-doc ack-policy->k (set/map-invert ack-policies))
 
 (def deliver-policies
   {:nats.deliver-policy/all DeliverPolicy/All
@@ -24,58 +26,17 @@
    :nats.deliver-policy/last-per-subject DeliverPolicy/LastPerSubject
    :nats.deliver-policy/new DeliverPolicy/New})
 
-(def deliver-policy->k (set/map-invert deliver-policies))
+(def ^:no-doc deliver-policy->k (set/map-invert deliver-policies))
 
 (def replay-policies
   {:nats.replay-policy/limits ReplayPolicy/Instant
    :nats.replay-policy/work-queue ReplayPolicy/Original})
 
-(def replay-policy->k (set/map-invert replay-policies))
+(def ^:no-doc replay-policy->k (set/map-invert replay-policies))
 
-(defn build-consumer-configuration
-  [{::keys [ack-policy ack-wait backoff deliver-group deliver-policy deliver-subject
-            description durable? filter-subject filter-subjects flow-control
-            headers-only? idle-heartbeat inactive-threshold max-ack-pending max-batch
-            max-bytes max-deliver max-expires max-pull-waiting mem-storage? metadata
-            num-replicas pause-until rate-limit replay-policy sample-frequency
-            start-sequence sequence start-time] :as opts}]
-  (let [consumer-name (::name opts)]
-    (assert (or (not durable?) (not (nil? consumer-name))) "Durable consumers must have a :nats.consumer/name")
-    (cond-> ^ConsumerConfiguration$Builder (ConsumerConfiguration/builder)
-      (ack-policies ack-policy) (.ackPolicy (ack-policies ack-policy))
-      ack-wait (.ackWait ack-wait)
-      backoff (.backoff backoff)
-      deliver-group (.deliverGroup deliver-group)
-      (deliver-policies deliver-policy) (.deliverPolicy (deliver-policies deliver-policy))
-      deliver-subject (.deliverSubject deliver-subject)
-      description (.description description)
-      durable? (.durable consumer-name)
-      filter-subject (.filterSubject filter-subject)
-      filter-subjects (.filterSubjects filter-subjects)
-      flow-control (.flowControl flow-control)
-      headers-only? (.headersOnly headers-only?)
-      idle-heartbeat (.idleHeartbeat idle-heartbeat)
-      inactive-threshold (.inactiveThreshold inactive-threshold)
-      max-ack-pending (.maxAckPending max-ack-pending)
-      max-batch (.maxBatch max-batch)
-      max-bytes (.maxBytes max-bytes)
-      max-deliver (.maxDeliver max-deliver)
-      max-expires (.maxExpires max-expires)
-      max-pull-waiting (.maxPullWaiting max-pull-waiting)
-      mem-storage? (.memStorage mem-storage?)
-      metadata (.metadata metadata)
-      (and consumer-name (not durable?)) (.name consumer-name)
-      num-replicas (.numReplicas num-replicas)
-      pause-until (.pauseUntil pause-until)
-      rate-limit (.rateLimit rate-limit)
-      (replay-policies replay-policy) (.replayPolicy (replay-policies replay-policy))
-      sample-frequency (.sampleFrequency sample-frequency)
-      start-sequence (.startSequence start-sequence)
-      sequence (.startSequence sequence)
-      start-time (.startTime (.atZone start-time nats/default-tz))
-      :then (.build))))
+;; Map data classes to maps
 
-(defn consumer-configuration->map [^ConsumerConfiguration config]
+(defn ^:no-doc consumer-configuration->map [^ConsumerConfiguration config]
   {::ack-policy-was-set? (.ackPolicyWasSet config)
    ::backoff-was-set? (.backoffWasSet config)
    ::deliver-policy-was-set? (.deliverPolicyWasSet config)
@@ -124,7 +85,7 @@
    ::replay-policy-was-set? (.replayPolicyWasSet config)
    ::start-seq-was-set? (.startSeqWasSet config)})
 
-(defn consumer-info->map [^ConsumerInfo info]
+(defn ^:no-doc consumer-info->map [^ConsumerInfo info]
   {::ack-floor (some-> (.getAckFloor info) .getLastActive)
    ::calculated-pending (.getCalculatedPending info)
    ::cluster-info (cluster/cluster-info->map (.getClusterInfo info))
@@ -142,13 +103,104 @@
    ::timestamp (some-> (.getTimestamp info) .toInstant)
    ::push-bound? (.isPushBound info)})
 
-(defn ^{:style/indent 1 :export true} create-consumer [conn configuration]
-  (->> (build-consumer-configuration configuration)
-       (.addOrUpdateConsumer (.jetStreamManagement conn) (::stream-name configuration))
+;; Build option classes
+
+(defn ^:no-doc build-consumer-configuration
+  [{::keys [ack-policy ack-wait backoff deliver-group deliver-policy deliver-subject
+            description durable? filter-subject filter-subjects flow-control
+            headers-only? idle-heartbeat inactive-threshold max-ack-pending max-batch
+            max-bytes max-deliver max-expires max-pull-waiting mem-storage? metadata
+            num-replicas pause-until rate-limit replay-policy sample-frequency
+            start-sequence sequence start-time] :as opts}]
+  (let [consumer-name (::name opts)]
+    (assert (or (not durable?) (not (nil? consumer-name))) "Durable consumers must have a :nats.consumer/name")
+    (cond-> ^ConsumerConfiguration$Builder (ConsumerConfiguration/builder)
+      (ack-policies ack-policy) (.ackPolicy (ack-policies ack-policy))
+      ack-wait (.ackWait ack-wait)
+      backoff (.backoff backoff)
+      deliver-group (.deliverGroup deliver-group)
+      (deliver-policies deliver-policy) (.deliverPolicy (deliver-policies deliver-policy))
+      deliver-subject (.deliverSubject deliver-subject)
+      description (.description description)
+      durable? (.durable consumer-name)
+      filter-subject (.filterSubject filter-subject)
+      filter-subjects (.filterSubjects filter-subjects)
+      flow-control (.flowControl flow-control)
+      headers-only? (.headersOnly headers-only?)
+      idle-heartbeat (.idleHeartbeat idle-heartbeat)
+      inactive-threshold (.inactiveThreshold inactive-threshold)
+      max-ack-pending (.maxAckPending max-ack-pending)
+      max-batch (.maxBatch max-batch)
+      max-bytes (.maxBytes max-bytes)
+      max-deliver (.maxDeliver max-deliver)
+      max-expires (.maxExpires max-expires)
+      max-pull-waiting (.maxPullWaiting max-pull-waiting)
+      mem-storage? (.memStorage mem-storage?)
+      metadata (.metadata metadata)
+      (and consumer-name (not durable?)) (.name consumer-name)
+      num-replicas (.numReplicas num-replicas)
+      pause-until (.pauseUntil pause-until)
+      rate-limit (.rateLimit rate-limit)
+      (replay-policies replay-policy) (.replayPolicy (replay-policies replay-policy))
+      sample-frequency (.sampleFrequency sample-frequency)
+      start-sequence (.startSequence start-sequence)
+      sequence (.startSequence sequence)
+      start-time (.startTime (.atZone start-time nats/default-tz))
+      :then (.build))))
+
+(defn ^:no-doc build-consume-options [{:keys [batch-bytes batch-size bytes threshold-pct]}]
+  (cond-> ^ConsumeOptions$Builder (ConsumeOptions/builder)
+    batch-bytes (.batchBytes batch-bytes)
+    batch-size (.batchSize batch-size)
+    bytes (.bytes bytes)
+    threshold-pct (.thresholdPercent threshold-pct)
+    :then (.build)))
+
+;; Public API
+
+(defn ^{:style/indent 1 :export true} create-consumer
+  "Create consumer. `config` is a map of:
+
+   - `:nats.consumer/name`
+   - `:nats.consumer/ack-policy`
+   - `:nats.consumer/ack-wait`
+   - `:nats.consumer/backoff`
+   - `:nats.consumer/deliver-group`
+   - `:nats.consumer/deliver-policy`
+   - `:nats.consumer/deliver-subject`
+   - `:nats.consumer/description`
+   - `:nats.consumer/durable?`
+   - `:nats.consumer/filter-subject`
+   - `:nats.consumer/filter-subjects`
+   - `:nats.consumer/flow-control`
+   - `:nats.consumer/headers-only?`
+   - `:nats.consumer/idle-heartbeat`
+   - `:nats.consumer/inactive-threshold`
+   - `:nats.consumer/max-ack-pending`
+   - `:nats.consumer/max-batch`
+   - `:nats.consumer/max-bytes`
+   - `:nats.consumer/max-deliver`
+   - `:nats.consumer/max-expires`
+   - `:nats.consumer/max-pull-waiting`
+   - `:nats.consumer/mem-storage?`
+   - `:nats.consumer/metadata`
+   - `:nats.consumer/num-replicas`
+   - `:nats.consumer/pause-until`
+   - `:nats.consumer/rate-limit`
+   - `:nats.consumer/replay-policy`
+   - `:nats.consumer/sample-frequency`
+   - `:nats.consumer/start-sequence`
+   - `:nats.consumer/sequence`
+   - `:nats.consumer/start-time`"
+  [conn config]
+  (->> (build-consumer-configuration config)
+       (.addOrUpdateConsumer (.jetStreamManagement conn) (::stream-name config))
        consumer-info->map))
 
-(defn ^{:style/indent 1 :export true} update-consumer [conn configuration]
-  (create-consumer conn configuration))
+(defn ^{:style/indent 1 :export true} update-consumer
+  "Update consumer. See `create-consumer` for keys in `config`."
+  [conn config]
+  (create-consumer conn config))
 
 (defn ^:export delete-consumer [conn stream-name consumer-name]
   (.deleteConsumer (.jetStreamManagement conn) stream-name consumer-name))
@@ -173,17 +225,14 @@
   (-> (.jetStreamManagement conn)
       (.resumeConsumer stream-name consumer-name)
       stream/stream-info->map))
-(defn build-consume-options [{:keys [batch-bytes batch-size bytes threshold-pct]}]
-  (cond-> ^ConsumeOptions$Builder (ConsumeOptions/builder)
-    batch-bytes (.batchBytes batch-bytes)
-    batch-size (.batchSize batch-size)
-    bytes (.bytes bytes)
-    threshold-pct (.thresholdPercent threshold-pct)
-    :then (.build)))
 
 (defn ^{:style/indent 1 :export true} subscribe
-  "Subscribe to messages on `stream-name` for `consumer-name`. Refer to
-  `build-consume-options` for keys in `opts`."
+  "Subscribe to messages on `stream-name` for `consumer-name`. `opts` is a map of:
+
+   - `:batch-bytes`
+   - `:batch-size`
+   - `:bytes`
+   - `:threshold-pct`"
   [conn stream-name consumer-name & [opts]]
   (-> (.getStreamContext conn stream-name)
       (.getConsumerContext consumer-name)
