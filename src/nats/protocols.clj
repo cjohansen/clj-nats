c2a1d7bd50ae947750a0fd5e3a0b9aa7173fa62e
Teodor Heggelund <git@teod.eu>
2026-07-10 14:18:14 +0200

    Print NATS connections as "#nats/connection <server-url>"

--- src/nats/protocols.clj
+++ src/nats/protocols.clj
@@ -1,4 +1,6 @@
-(ns nats.protocols)
+(ns nats.protocols
+  (:import (io.nats.client Connection)
+           (java.io Writer)))
 
 (defprotocol JNatsConnectionWrapper
   (get-jnats-conn ^io.nats.client.Connection [self]))
@@ -6,3 +8,10 @@
 (defprotocol NatsConnConfig
   (get-configuration [self])
   (configure! [self k v]))
+
+(defmethod print-method nats.protocols.JNatsConnectionWrapper
+  [conn writer]
+  (let [^Connection jnats-conn (get-jnats-conn conn)]
+    (Writer/.write writer "#nats/connection ")
+    (Writer/.write writer (pr-str (or (.getConnectedUrl jnats-conn)
+                                      "<disconnected>")))))
